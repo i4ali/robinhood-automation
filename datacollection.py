79250a095ef0ea0a3613d209e30399f92ed30a52
Imran.Ali <ali.muhammadimran@gmail.com>
2021-12-02 14:44:17 -0600

    fully working P/L report on each ticker

--- datacollection.py
+++ datacollection.py
@@ -1,24 +1,46 @@
 from robin_stocks import robinhood
 from csv import writer
+import pandas as pd
 
 password = input ("Enter password :")
 robinhood.login(username='ali.muhammadimran@gmail.com', password=password)
 
 robinhood.export.export_completed_option_orders('.', 'completed_options.csv')
 robinhood.export.export_completed_stock_orders('.', 'completed_stocks.csv')
-all_orders = robinhood.account.build_holdings(with_dividends=False)
-with open('open_positions.csv', 'w', newline='') as f:
+# all_orders = robinhood.account.build_holdings(with_dividends=False)
+# with open('open_positions.csv', 'w', newline='') as f:
+#     csv_writer = writer(f)
+#     csv_writer.writerow([
+#         'symbol',
+#         'average_price',
+#         'quantity'
+#     ])
+#     for symbol, data in all_orders.items():
+#         csv_writer.writerow([
+#             symbol,
+#             data['average_buy_price'],
+#             data['quantity'],
+#         ])
+#     f.close()
+
+event_rows = []
+df_stocks = pd.read_csv('completed_stocks.csv')
+ticker_list = list(df_stocks['symbol'].unique())
+for ticker in ticker_list:
+    events = robinhood.get_events(ticker)
+    for event in events:
+        event_row = [ticker, event['event_date'], event['type'], 'buy' if event['direction'] == 'debit' else 'sell',
+                     '0.00', float(event['quantity']) * 100, float(event['total_cash_amount']) / 100]
+        event_rows.append(event_row)
+
+with open('completed_stocks.csv', 'a', newline='') as f:
     csv_writer = writer(f)
-    csv_writer.writerow([
-        'symbol',
-        'average_price',
-        'quantity'
-    ])
-    for symbol, data in all_orders.items():
-        csv_writer.writerow([
-            symbol,
-            data['average_buy_price'],
-            data['quantity'],
-        ])
+    for event_row in event_rows:
+        csv_writer.writerow(event_row)
     f.close()
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pandas as pd
+from robin_stocks import robinhood
 
 ticker = input("Enter ticker :")
 
-df = pd.read_csv('completed_options.csv')
-df3 = pd.read_csv('open_positions.csv')
+df_options = pd.read_csv('completed_options.csv')
+df_positions = pd.read_csv('open_positions.csv')
+df_stocks = pd.read_csv('completed_stocks.csv')
+
 
 # options short position calculations
-df_filtered_short_positions = df[(df['opening_strategy'] != 'long_put') & (df['opening_strategy'] != 'long_call') & (
-        df['closing_strategy'] != 'long_put') & (df['closing_strategy'] != 'long_call')]
+df_filtered_short_positions = df_options[(df_options['opening_strategy'] != 'long_put') & (df_options['opening_strategy'] != 'long_call') & (
+        df_options['closing_strategy'] != 'long_put') & (df_options['closing_strategy'] != 'long_call')]
 df_filtered_short_positions_instrument = df_filtered_short_positions[
     df_filtered_short_positions['chain_symbol'] == ticker.upper()]
 df_filtered_short_positions_instrument_credit = df_filtered_short_positions_instrument[
@@ -23,22 +26,44 @@ df_filtered_short_positions_instrument_debit['total_price_debit'] = df_filtered_
                                                                         'price'] * \
                                                                     df_filtered_short_positions_instrument_debit[
                                                                         'processed_quantity']
-total_credit = df_filtered_short_positions_instrument_credit['total_price_credit'].sum() * 100
-total_debit = df_filtered_short_positions_instrument_debit['total_price_debit'].sum() * 100
+total_credit_options = df_filtered_short_positions_instrument_credit['total_price_credit'].sum() * 100
+total_debit_options = df_filtered_short_positions_instrument_debit['total_price_debit'].sum() * 100
 
-# open positions calculations
-df_filtered_open_position = df3[df3['symbol'] == ticker.upper()]
-if len(df_filtered_open_position) > 0:
-    total_cost_of_owning_ticker = \
-        (df_filtered_open_position['average_price'] * df_filtered_open_position['quantity']).values[0]
-else:
-    total_cost_of_owning_ticker = None
 
-pnl_options_selling = total_credit - total_debit
+# average stock price
+df_stocks_filtered_ticker = df_stocks[
+    df_stocks['symbol'] == ticker.upper()]
+
+df_stocks_filtered_ticker_buy = df_stocks_filtered_ticker[df_stocks_filtered_ticker['side'] == 'buy']
+
+df_stocks_filtered_ticker_sell = df_stocks_filtered_ticker[df_stocks_filtered_ticker['side'] == 'sell']
+
+df_stocks_filtered_ticker_buy['total_price_debit'] = df_stocks_filtered_ticker_buy['average_price'] * df_stocks_filtered_ticker_buy['quantity']
+
+df_stocks_filtered_ticker_sell['total_price_credit'] = df_stocks_filtered_ticker_sell['average_price'] * df_stocks_filtered_ticker_sell['quantity']
+
+total_debit_stocks = df_stocks_filtered_ticker_buy['total_price_debit'].sum()
+total_stocks_bought = df_stocks_filtered_ticker_buy['quantity'].sum()
+
+total_credit_stocks = df_stocks_filtered_ticker_sell['total_price_credit'].sum()
+
+
+# # open positions calculations
+# df_filtered_open_position = df_positions[df_positions['symbol'] == ticker.upper()]
+# if len(df_filtered_open_position) > 0:
+#     total_cost_of_owning_ticker = \
+#         (df_filtered_open_position['average_price'] * df_filtered_open_position['quantity']).values[0]
+# else:
+#     total_cost_of_owning_ticker = None
+
+pnl_options_selling = total_credit_options - total_debit_options
+pnl_stocks = total_credit_stocks - total_debit_stocks
+average_cost_stock = total_debit_stocks / total_stocks_bought
 
 print('****')
-print('P/L from selling options on %s: %s' % (ticker.upper(), pnl_options_selling))
-if total_cost_of_owning_ticker:
-    print('Average cost of owning %s (without option selling): %s' % (ticker.upper(), total_cost_of_owning_ticker))
-    print('Average cost of owning %s: %s' % (ticker.upper(), (total_cost_of_owning_ticker - pnl_options_selling)))
+print('Total P/L on %s: %f' % (ticker.upper(), pnl_options_selling+pnl_stocks))
+print('P/L from selling options on %s: %f' % (ticker.upper(), pnl_options_selling))
+print('P/L from stocks on %s: %f' % (ticker.upper(), pnl_stocks))
+print('Average cost of owning %s: %f' % (ticker.upper(), (average_cost_stock)))
+print('Average cost of owning(with option selling) %s: %f' % (ticker.upper(), (average_cost_stock*100-pnl_options_selling)/100))
 print('****')
